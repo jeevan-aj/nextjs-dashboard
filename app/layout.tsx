4dbbf7ced672fb7650359b6cf564933e2a53792d
jeevan-aj <jeevan@finanshels.com>
2024-08-28 19:50:47 +0530

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
-export default function RootLayout({
-  children,
-}: {
+import './ui/global.css'
+
+import {poppins} from './ui/fonts'
+
+export default function RootLayout({children,}: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${poppins.className} antialiased`}>{children}</body>
     </html>
   );
 }
